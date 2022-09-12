f260f9911cac80106e7b471e777a7ac7bb76d871
Thanh <cbxhghost@gmail.com>
2022-09-12 14:47:06 +0700

    file animate

---
+++ index.js
@@ -0,0 +1,6 @@
+function click1() {
+  var click = document.getElementById("click");
+  var rs = (document.getElementById("reset").style.display = "block");
+  var rs = (document.getElementById("reset").style.justifyContent = "center");
+  var rs = (document.getElementById("reset").style.alignItems = "center");
+}
